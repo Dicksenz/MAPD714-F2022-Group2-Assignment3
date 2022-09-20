dd22223c8ecf750427abf8ff4d1b893c8124ae84
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-09-20 12:49:33 -0400

    initial commit

---
+++ MAPD714F2022-group2-assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD714F2022-group2-assignment1
+//
+//  Created by Dicksen Veloopillay on 2022-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
